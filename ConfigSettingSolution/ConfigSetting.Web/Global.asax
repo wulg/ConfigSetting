49005826bdfebf66e316c442f0fa31ae9964db75
wuliang <wuliang@Ctrip.com>
2014-12-31 14:37:49 +0800

    rename

---
+++ ConfigSettingSolution/ConfigSetting.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConfigSetting.Web.MvcApplication" Language="C#" %>
---
+++ ConfigSettingSolution/ConfigSetting.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConfigSetting.Web.Startup))]
+namespace ConfigSetting.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
